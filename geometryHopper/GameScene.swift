b0da20552ace76dfc482adb8ac47a5c1d42cec09
tsukudabuddha <andrewtsukuda@gmail.com>
2018-03-20 15:56:39 -0700

    Switched back toi using phhyics to control character not skactions

--- geometryHopper/GameScene.swift
+++ geometryHopper/GameScene.swift
@@ -16,7 +16,6 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     
     override func didMove(to view: SKView) {
         setupScence()
-        
     }
     
     
@@ -26,26 +25,122 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         playerMovement()
     }
     
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+//        player.removeAction(forKey: "jumpSequence")
+//        var dVector = CGVector.zero
+//        var downVector = CGVector.zero
+//        let forceValue = 125
+//
+//        switch player.orientation {
+//        case .bottom:
+//            dVector = CGVector(dx: 0, dy: forceValue)
+//            downVector = CGVector(dx: 0, dy: -1 * player.frame.minY)
+//        case .right:
+//            dVector = CGVector(dx: forceValue * -1, dy: 0)
+//            downVector = CGVector(dx: -1 * player.frame.maxX, dy: 0)
+//        case .top:
+//            dVector = CGVector(dx: 0, dy: forceValue * -1)
+//            downVector = CGVector(dx: 0, dy: player.frame.maxY)
+//        case .left:
+//            dVector = CGVector(dx: forceValue, dy: 0)
+//            downVector = CGVector(dx: player.frame.minX, dy: 0)
+//        }
+//        let jumpAction = SKAction.move(by: dVector, duration: 0.35)
+//        jumpAction.timingMode = SKActionTimingMode.easeOut
+//        let down = SKAction.run {
+//            switch self.player.orientation {
+//            case .bottom:
+//                downVector = CGVector(dx: 0, dy: self.frame.minY - self.player.frame.minY)
+//            case .right:
+//                downVector = CGVector(dx:  self.frame.maxX - self.player.frame.maxX, dy: 0)
+//            case .top:
+//                downVector = CGVector(dx: 0, dy: self.frame.maxY - self.player.frame.maxY)
+//            case .left:
+//                downVector = CGVector(dx: self.frame.minX - self.player.frame.minX, dy: 0)
+//            }
+//            let down = SKAction.move(by: downVector, duration: 0.35)
+//            down.timingMode = .easeIn
+//            self.player.run(down)
+//        }
+//
+//        down.timingMode = .easeIn
+//        let sequence = SKAction.sequence([jumpAction, down])
+//        player.run(sequence, withKey: "jumpSequence")
+        let force: CGFloat = 1000000
+        var impulse = CGVector.zero
+        
+        switch self.player.orientation {
+        case .bottom:
+            impulse = CGVector(dx: 0, dy: force)
+        case .right:
+            impulse = CGVector(dx: force * -1, dy: 0)
+        case .top:
+            impulse = CGVector(dx: 0, dy: force * -1)
+        case .left:
+            impulse = CGVector(dx: force, dy: 0)
+        }
+        player.physicsBody?.applyImpulse(impulse)
+        
+    }
+    
     func playerMovement() {
         let interval: CGFloat = 3.8
-        let dy = (self.frame.maxY - player.frame.width) / interval / 60 // 60 fps
-        let dx = (self.frame.maxX - player.frame.width) / interval / 60
-        
+        let dy = (self.frame.maxY - (player.frame.width / 2)) / interval / 60 // 60 fps
+        let dx = (self.frame.maxX - (player.frame.width / 2)) / interval / 60
+        var gravity = CGVector.zero
+
         switch player.orientation {
         case .bottom:
+            
+            /* Move Player */
             player.position.x += dx
-            player.physicsBody?.applyForce(CGVector(dx: 0, dy: -9.8))
+            
+            /* Set Gravity to normal */
+            gravity = CGVector(dx: 0, dy: -9.8)
+            
+            if player.frame.maxX > self.frame.width - 5 {
+                /* Change player orientation to work with new gravity */
+                player.orientation = .right
+            }
         case .right:
+            
+            /* Move Player */
             player.position.y += dy
-            player.physicsBody?.applyForce(CGVector(dx: 9.8, dy: 0))
+            
+            /* Set Gravity to right */
+            gravity = CGVector(dx: 9.8, dy: 0)
+            
+            if player.frame.maxY > self.frame.height - 5 {
+                /* Change player orientation */
+                changeWalls()
+            }
+        
         case .top:
+            /* Move Player */
             player.position.x -= dx
-            player.physicsBody?.applyForce(CGVector(dx: 0, dy: 9.8))
+            
+            /* Set Gravity to top */
+            gravity = CGVector(dx: 0, dy: 9.8)
+            
+            if player.frame.minX < 5 {
+                /* Change player orientation */
+                changeWalls()
+            }
+    
         case .left:
+            /* Move Player */
             player.position.y -= dy
-            player.physicsBody?.applyForce(CGVector(dx: -9.8, dy: 0))
+            
+            /* Set Gravity to left */
+            gravity = CGVector(dx: -9.8, dy: 0)
+            
+            if player.frame.minY < 5 {
+                /* Change player orientation */
+                changeWalls()
+            }
         }
         
+        player.physicsBody?.applyForce(gravity)
     }
     
     func setupScence() {
@@ -66,10 +161,10 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         physicsWorld.gravity = CGVector.zero
         
         /* Setup Timer to change 'gravity' */
-        wallTimer = Timer.scheduledTimer(timeInterval: 3.8, target: self, selector: #selector(changeWalls), userInfo: nil, repeats: true)
+//        wallTimer = Timer.scheduledTimer(timeInterval: 3.8, target: self, selector: #selector(changeWalls), userInfo: nil, repeats: true)
     }
     
-    @objc func changeWalls() {
+    func changeWalls() {
         switch player.orientation {
         case .bottom:
             player.orientation = .right
--- geometryHopper/Player.swift
+++ geometryHopper/Player.swift
@@ -9,6 +9,30 @@
 import Foundation
 import SpriteKit
 
-struct Player: SKShapeNode {
+enum ScreenSide {
+    case bottom, right, top, left
+}
+
+class Player: SKShapeNode {
+    var orientation: ScreenSide = .bottom
     
+    override init(){
+        super.init()
+        
+        let circleOfRadius: CGFloat = 30
+        let diameter = circleOfRadius * 2
+        self.path = CGPath(ellipseIn: CGRect(origin: CGPoint(x: circleOfRadius * -1, y: circleOfRadius * -1), size: CGSize(width: diameter, height: diameter)), transform: nil)
+        
+        self.strokeColor = UIColor.white
+        self.fillColor = UIColor.blue
+        self.physicsBody = SKPhysicsBody(circleOfRadius: circleOfRadius, center: CGPoint.zero)
+        self.physicsBody?.allowsRotation = true
+        self.physicsBody?.mass = 100
+        self.physicsBody?.affectedByGravity = false
+        self.run(SKAction.rotate(byAngle: 1, duration: 0.1))
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
